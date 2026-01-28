4396352db3467b74898b20d6793e244a59ef0781
sanjanav2102 <sanjanav2102@users.noreply.github.com>
2026-01-28 19:39:59 +0530

    Issue in deployment2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+//import { HashRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
